506035d2389a385c81aae171de5b7c4f9fd35ee5
crystalDf <chendong333@gmail.com>
2016-09-08 22:49:19 +0800

    Chapter 13 PDF and Word

---
+++ reading_word_documents_test.py
@@ -0,0 +1,11 @@
+import docx
+
+doc = docx.Document('demo.docx')
+print(len(doc.paragraphs))
+print(doc.paragraphs[0].text)
+print(doc.paragraphs[1].text)
+print(len(doc.paragraphs[1].runs))
+print(doc.paragraphs[1].runs[0].text)
+print(doc.paragraphs[1].runs[1].text)
+print(doc.paragraphs[1].runs[2].text)
+print(doc.paragraphs[1].runs[3].text)
