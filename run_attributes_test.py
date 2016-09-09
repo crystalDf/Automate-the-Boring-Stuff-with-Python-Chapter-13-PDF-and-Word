3db5e71e2c57834b0bf67c6e51786f1ce956d0d1
crystalDf <chendong333@gmail.com>
2016-09-09 22:30:41 +0800

    Chapter 13 PDF and Word

---
+++ run_attributes_test.py
@@ -0,0 +1,13 @@
+import docx
+
+doc = docx.Document('demo.docx')
+print(doc.paragraphs[0].text)
+print(doc.paragraphs[0].style)
+doc.paragraphs[0].style = 'Normal'
+print(doc.paragraphs[1].text)
+print((doc.paragraphs[1].runs[0].text, doc.paragraphs[1].runs[1].text,
+       doc.paragraphs[1].runs[2].text, doc.paragraphs[1].runs[3].text))
+doc.paragraphs[1].runs[0].style = 'QuoteChar'
+doc.paragraphs[1].runs[1].underline = True
+doc.paragraphs[1].runs[3].underline = True
+doc.save('restyled.docx')
